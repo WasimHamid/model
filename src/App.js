7742c1c7b1b85f6c0acbdc8af7b70a507b92970e
Your Name <you@example.com>
2019-04-12 17:11:58 +0100

    added model

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+
+import Modal from "../src/model/model";
+
+import "./App.css";
 
 class App extends Component {
+  state = { show: false };
+
+  changeShow = () => {
+    this.setState(state => ({ show: !state.show }));
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <button onClick={this.changeShow}>Show</button>
+          {this.state.show && <Modal onClose={this.changeShow} />}
         </header>
       </div>
     );
---
+++ src/model/model.js
@@ -0,0 +1,29 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import css from "./Portal.module.css";
+
+class Modal extends React.Component {
+  render() {
+    return ReactDOM.createPortal(
+      <div onClick={this.props.onClose} className={css.container}>
+        <div
+          onClick={event => {
+            event.stopPropagation();
+          }}
+          className={css.modal}
+        >
+          <h2>PORTAL!</h2>
+          <p>
+            You can do some stuff in here, and we have attached this whole this
+            to the body, not the react tree
+          </p>
+          <button onClick={this.props.onClose}>Close</button>
+        </div>
+      </div>,
+      document.body
+    );
+  }
+}
+
+export default Modal;
